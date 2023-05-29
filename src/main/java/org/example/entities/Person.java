3b81a1a431a1fac0c08eb4b4b49c406e580b6522
ivanov.io <iliya00817@gmail.com>
2023-05-29 10:43:53 +0300

    Hibernate task

--- src/main/java/org/example/entities/Person.java
+++ src/main/java/org/example/entities/Person.java
@@ -1,6 +1,12 @@
 package org.example.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
