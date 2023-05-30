1f9647248b462d2384a706f5e7b1be8011d7fadb
ivanov.io <iliya00817@gmail.com>
2023-05-30 17:00:32 +0300

    JDBC task

--- src/main/java/org/example/JdbcConfig.java
+++ src/main/java/org/example/JdbcConfig.java
@@ -1,15 +1,21 @@
 package org.example;
 
-import org.postgresql.ds.PGSimpleDataSource;
-
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class JdbcConfig {
-    public static DataSource createDataSource() {
+    public static Connection createConnection() {
         final String url =
                 "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=postgres";
-        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl(url);
-        return dataSource;
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "postgres");
+        try {
+            return DriverManager.getConnection(url, props);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/example/JdbcTask.java
+++ src/main/java/org/example/JdbcTask.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,10 +10,10 @@ public class JdbcTask {
     public static final String SELECT_FROM_PERSON = "SELECT * FROM high_task.person";
     public static final String UPDATE_BANK_NAME = "UPDATE high_task.bank SET name = ? where id = ?";
     public static final String SELECT_COUNT_BANK = "SELECT COUNT(*) as count FROM high_task.bank";
-    private final DataSource dataSource = JdbcConfig.createDataSource();
+    private final Connection connection = JdbcConfig.createConnection();
 
     public void getPersons() {
-        try (Connection connection = dataSource.getConnection();
+        try (connection;
              PreparedStatement statement = connection.prepareStatement(SELECT_FROM_PERSON)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -30,7 +29,7 @@ public class JdbcTask {
     }
 
     public void updateBanks() {
-        try (Connection connection = dataSource.getConnection();
+        try (connection;
              PreparedStatement updatePS = connection.prepareStatement(UPDATE_BANK_NAME);
              PreparedStatement selectPS = connection.prepareStatement(SELECT_COUNT_BANK)) {
             connection.setAutoCommit(false);
